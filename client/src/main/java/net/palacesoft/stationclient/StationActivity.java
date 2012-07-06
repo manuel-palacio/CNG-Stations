bf92d4d9b795085f76562bb2cf6040fc27312a97
Manuel Palacio <emanuel.palacio@gmail.com>
2012-07-07 01:19:54 +0200

    added a loading dialog

--- client/src/main/java/net/palacesoft/stationclient/StationActivity.java
+++ client/src/main/java/net/palacesoft/stationclient/StationActivity.java
@@ -1,17 +1,20 @@
 package net.palacesoft.stationclient;
 
+import android.app.ProgressDialog;
 import android.location.*;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import com.fuelstation.R;
-import net.palacesoft.stationclient.mapoverlay.StationOverlay;
 import com.google.android.maps.*;
+import net.palacesoft.stationclient.mapoverlay.StationOverlay;
+import net.palacesoft.stationclient.mapoverlay.StationOverlayItem;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,12 +22,12 @@ public class StationActivity extends MapActivity {
 
     private MapView mapView;
     private MapController mapController;
-    private Handler guiThread;
     private StationOverlay stationOverlay;
     private Location currentLocation;
     private String country;
     private MyLocationOverlay myLocationOverlay;
     private RestTemplate restTemplate = new RestTemplate();
+
 
 
     @Override
@@ -42,7 +45,6 @@ public class StationActivity extends MapActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
         return true;
-
     }
 
     @Override
@@ -58,13 +60,23 @@ public class StationActivity extends MapActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        myLocationOverlay.disableMyLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myLocationOverlay.enableMyLocation();
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         initMapView();
-
-        guiThread = new Handler();
 
         stationOverlay = new StationOverlay(this.getResources().getDrawable(R.drawable.marker), this);
 
@@ -86,7 +98,6 @@ public class StationActivity extends MapActivity {
     private void initMyLocation() {
         myLocationOverlay = new MyLocationOverlay(this, mapView);
         myLocationOverlay.enableMyLocation();
-        myLocationOverlay.enableCompass();
         mapView.getOverlays().add(myLocationOverlay);
 
         myLocationOverlay.runOnFirstFix(new Runnable() {
@@ -131,7 +142,65 @@ public class StationActivity extends MapActivity {
     public void getStationsFromCloud(String country) {
 
         if (country != null) {
-            guiThread.post(new UpdateStationInfoTask(restTemplate, stationOverlay, country));
+            new LoadViewTask().execute(country);
+        }
+    }
+
+    private class LoadViewTask extends AsyncTask<String, Integer, List<StationOverlayItem>> {
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            //Create a new progress dialog
+            progressDialog = new ProgressDialog(StationActivity.this);
+            //Set the progress dialog to display a horizontal progress bar
+           // progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            //Set the dialog title to 'Loading...'
+           // progressDialog.setTitle("Laddar...");
+            //Set the dialog message to 'Loading application View, please wait...'
+            progressDialog.setMessage("Laddar...");
+            //This dialog can't be canceled by pressing the back key
+            progressDialog.setCancelable(false);
+            //This dialog isn't indeterminate
+       //     progressDialog.setIndeterminate(true);
+            //The maximum number of items is 100
+            //progressDialog.setMax(100);
+            //Set the current progress to zero
+            //progressDialog.setProgress(0);
+            //Display the progress dialog
+            progressDialog.show();
+        }
+
+        @Override
+        protected List<StationOverlayItem> doInBackground(String... params) {
+            String url = "http://fuelstationservice.appspot.com/stations/country/{query}";
+            StationDTO[] stations = restTemplate.getForObject(url, StationDTO[].class, params[0].toUpperCase());
+            List<StationOverlayItem> stationOverlayItems = new ArrayList<StationOverlayItem>();
+            for (StationDTO stationDTO : stations) {
+                //  Log.i(StationActivity.class.getName(), stationDTO.getStreet());
+
+                float lat = stationDTO.getLatitude();
+                float lng = stationDTO.getLongitude();
+
+                GeoPoint point = new GeoPoint((int) (lat * 1E6), (int) (lng * 1E6));
+                String address = stationDTO.getStreet() + " (" + stationDTO.getCity() + ")";
+                StationOverlayItem overlayItem = new StationOverlayItem(point, address, "", stationDTO);
+                stationOverlayItems.add(overlayItem);
+
+            }
+            return stationOverlayItems;
+        }
+
+        @Override
+        protected void onPostExecute(List<StationOverlayItem> result) {
+            //close the progress dialog
+
+            for (StationOverlayItem next : result) {
+                stationOverlay.addOverlay(next);
+            }
+
+
+            progressDialog.dismiss();
         }
     }
 }
