68a0680d7b91967cd9f67da6f0a8cb9ab794c66c
Manuel Palacio <emanuel.palacio@gmail.com>
2012-07-28 12:16:26 +0200

    added license

--- client/src/main/java/net/palacesoft/cngstation/client/StationActivity.java
+++ client/src/main/java/net/palacesoft/cngstation/client/StationActivity.java
@@ -1,3 +1,21 @@
+/**
+ * *******************************************************************************************************************
+ * <p/>
+ * Copyright (C) 7/28/12 by Manuel Palacio
+ * <p/>
+ * **********************************************************************************************************************
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * <p/>
+ * **********************************************************************************************************************
+ */
 package net.palacesoft.cngstation.client;
 
 import android.app.ProgressDialog;
@@ -121,7 +139,7 @@ public class StationActivity extends MapActivity {
                     Integer zoomLevel = null;
                     List<Address> addresses = new ArrayList<Address>();
                     if (city.equalsIgnoreCase("Alla")) {
-                        Address address = Country.findAddress(countries.getSelectedItem().toString()).getAddress();
+                        Address address = Country.findAddress(CountryCode.valueOf(countries.getSelectedItem().toString())).getAddress();
                         addresses.add(address);
                         zoomLevel = 6;
                     } else {
